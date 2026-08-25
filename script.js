884fbda81e2ba860170a67199c6fd3212223b236
raitetae-glitch <raitetae@gmail.com>
2026-08-25 09:30:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener(’DOMContentLoaded‘,function () {
    document.getElementById(’year‘).innerHTML = new Date().getFullYear();
-   console.log(’Portfolio ของ Narawit พร้อมแล้ว!‘);
+   console.log(’Portfolio ของ phanupong พร้อมแล้ว!‘);
 });
