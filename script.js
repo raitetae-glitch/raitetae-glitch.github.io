5284b50373f19c81e9e317bc5e6c9fe3106b8a24
raitetae-glitch <raitetae@gmail.com>
2026-08-25 09:04:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใล้ (Optional)
 document.addEventListener('DOMContentLoaded',function () {
+  document.getElementById('year').innerHTML = new Data().getFullYear();
   console.log('Portfolio ของ ภานุพงศ์ พร้อมแล้ว!');
 }};
